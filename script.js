bef9809781ec89373da35110a85f33cc683aa780
Shane Pearson <bubbafoley@gmail.com>
2025-11-24 21:55:13 -0600

    Add newsletter sign-up form with success message and illustrations
    - Created index.html for the newsletter sign-up form layout.
    - Added mobile and desktop SVG illustrations for the sign-up form.
    - Implemented form validation and success message display in script.js.
    - Designed styles for the form and success message in style.css.
    - Included design assets for active and error states, and desktop/mobile designs.
    - Established a style guide for layout, colors, and typography.

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("signup-form");
+    const emailInput = document.getElementById("email");
+    const errorMessage = document.getElementById("error-message");
+    const emailFormSection = document.getElementById("sign-up-form");
+    const successMessageSection = document.getElementById("success-message");
+    const mobileImaege = document.querySelector(".illustration-mobile");
+    const desktopImage = document.querySelector(".illustration-desktop");
+    const dismissButton = document.getElementById("dismiss-message");
+
+    dismissButton.addEventListener("click", () => {
+        successMessageSection.classList.add("hidden");
+        emailFormSection.classList.remove("hidden");
+        mobileImaege.classList.remove("hidden");
+        form.reset();
+    });
+
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const emailValue = emailInput.value.trim();
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailPattern.test(emailValue)) {
+            emailInput.classList.add("error");
+            errorMessage.style.display = "block";
+        } else {
+            mobileImaege.classList.add("hidden");
+            emailInput.classList.remove("error");
+            errorMessage.style.display = "none";
+            emailFormSection.classList.add("hidden");
+            successMessageSection.classList.remove("hidden");
+            form.reset();
+        }
+    });
+});
